de752aac8d0211a1d42665b40d3e08a09a2a5b3d
alihshmi <120030412+alihshmi@users.noreply.github.com>
2022-12-27 13:33:20 +0500

    Create calls.dart

---
+++ calls.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/material.dart';
+
+class calls extends StatelessWidget {
+  const calls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       backgroundColor: Colors.blueGrey.shade100,
+       body: SingleChildScrollView(child: Column(
+      children: const [
+          ListTile(
+           
+          leading: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/1.jpg'),
+               ),
+            ),
+               title: Text("031234567812"),
+                subtitle: Text("7minutes ago"),
+                 trailing: Icon(
+                    Icons.call,
+                  color: Color.fromARGB(255, 182, 10, 38),
+                 ),
+              ),
+              
+                 ListTile(
+           
+            leading: CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/3.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SAlih"),
+                subtitle: Text("Today,6.24pm"),
+               trailing: Icon(Icons.video_call,
+               color: Colors.red,),
+              ),
+               ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("09876354"),
+                subtitle: Text("(4)December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/16.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.video_call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+               ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+               ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+                ListTile(
+           
+            leading:  CircleAvatar(
+              radius: 50,
+              backgroundColor: Color.fromARGB(255, 164, 9, 253),
+              child: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage('images/10.jpg'),
+                // fit: BoxFit.fill
+              ),
+            ),
+               title: Text("SomeOne"),
+                subtitle: Text("December2,3:45pm"),
+              
+                trailing: Icon(Icons.call,
+               color: Colors.red,),
+              ),
+              
+           
+          
+
+
+      ]))
+    );
+  }
+}
